9933afd62aa1c1c26d1a04e57958be6ea2890d04
vagif14 <108517425+vagif14@users.noreply.github.com>
2023-02-14 19:32:12 +0400

    created develop

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { useLangInit } from "./lang/LangInit";
-import "./index.css";
 
 const Main = () => {
   useLangInit();
