f0f22da154bdbcc6c58ee56f0d4adea9aceef9ba
vagif14 <108517425+vagif14@users.noreply.github.com>
2023-02-14 19:30:14 +0400

    ✅ Added AntdUi ✅ Addedd other feature related to app

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { useLangInit } from "./lang/LangInit";
-import "./index.css";
+
+import "./assets/scss/main.scss";
 
 const Main = () => {
   useLangInit();
